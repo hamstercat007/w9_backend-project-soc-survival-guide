c764706207c9cb788ff111f303f3f7a9fd2cc565
MumtazO <mumtazob@gmail.com>
2022-06-21 14:31:04 +0100

    models folder created with get all resources function

--- models/resources.js
+++ models/resources.js
@@ -1,2 +1,16 @@
 const pool = require('../db/index');
+require('dotenv').config()
 
+/* GET all RESOURCES */
+
+async function resources () {
+    const response = await pool.query("SELECT * FROM resources")
+    const result = 
+    {
+    success: true,
+    payload: response.rows
+    }
+  return result  
+}
+
+module.exports = resources
--- routes/resources.js
+++ routes/resources.js
@@ -3,11 +3,13 @@
 */
 
 var express = require('express');
+const resources = require('../models/resources');
 var router = express.Router();
 
 /* GET resources listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', async function(req, res, next) {
+    const responseObject = await resources()
+  res.json(responseObject);
 });
 
 module.exports = router;
