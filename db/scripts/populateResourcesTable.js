c56825d0fa5f13bfd70cf9692d88d7a95ee7c820
MiguelLamas <lamasmigs@yahoo.co.uk>
2022-06-21 12:04:17 +0100

    library, data array completed and populateTable

--- db/scripts/populateResourcesTable.js
+++ db/scripts/populateResourcesTable.js
@@ -1,4 +1,5 @@
 const pool = require("../index.js");
+const resourcesData = require("../../lib/resourcesData");
 
 const queryString = "INSERT INTO resources (headline, description, url, category, format) VALUES ($1, $2, $3, $4, $5) RETURNING *; "
 
@@ -6,7 +7,7 @@ async function populateResourcesTable() {
     for(let i=0; i < resourcesData.length; i++) {
         const queryParams = [resourcesData[i].headline, resourcesData[i].description, resourcesData[i].url, resourcesData[i].category,resourcesData[i].format];
         let response = await pool.query(queryString, queryParams)
-        console.log(res.command)
+        console.log(response.command)
     }
 }
 
